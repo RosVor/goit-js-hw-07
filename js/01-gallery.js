cdb5641c1b56f5fb292f459412788b477cf6f319
RosVor <ros.vorokh@gmail.com>
2023-06-21 23:13:09 +0300

    beginnig

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
 
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 console.log(galleryItems);
