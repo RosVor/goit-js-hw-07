e594302de69b1aa6ec45467f2ea864e745a396ee
RosVor <ros.vorokh@gmail.com>
2023-06-21 23:17:26 +0300

    preview

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryList = document.querySelector('.gallery');
-
 const createGalleryItem = ({ preview, original, description }) => {
   return `
     <li class="gallery__item">
       <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
       </a>
     </li>
   `;
@@ -14,4 +18,12 @@ const createGalleryItem = ({ preview, original, description }) => {
 
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-console.log(galleryItems);
+
+const galleryImages = document.querySelectorAll('.gallery__image');
+galleryImages.forEach((image) => {
+  image.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+
