03095f9e751e3b9f0115ce40a063c6d693a33dbe
RosVor <ros.vorokh@gmail.com>
2023-07-04 20:59:32 +0300

    restart

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const gallery = document.querySelector('.gallery');
-    const galleryMarkup = galleryItems
-      .map(item => `
-        <li class="gallery__item">
-          <a class="gallery__link" href="${item.original}">
-            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-          </a>
-        </li>
-      `)
-      .join('');
   
-    gallery.innerHTML = galleryMarkup;
-
-    const lightbox = new SimpleLightbox('.gallery a', {
+    // Render gallery items
+    galleryItems.forEach(item => {
+      const listItem = document.createElement('li');
+      listItem.classList.add('gallery__item');
+  
+      const link = document.createElement('a');
+      link.classList.add('gallery__link');
+      link.href = item.original;
+  
+      const image = document.createElement('img');
+      image.classList.add('gallery__image');
+      image.src = item.preview;
+      image.alt = item.description;
+  
+      link.appendChild(image);
+      listItem.appendChild(link);
+      gallery.appendChild(listItem);
+    });
+  
+    // Initialize SimpleLightbox
+    new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionDelay: 250,
       captionsData: 'alt',
       captionPosition: 'bottom',
-      captionDelay: 250,
-      captionClass: 'caption-style',
-      animationSlide: true,
-      animationSpeed: 250,
-      history: true,
+      captionClass: 'caption',
     });
   });
