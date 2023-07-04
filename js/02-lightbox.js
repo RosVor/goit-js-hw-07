c6a75570eef5b70acbf82f44683b185992a57632
RosVor <ros.vorokh@gmail.com>
2023-07-04 21:14:48 +0300

    ???????

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,2 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-document.addEventListener('DOMContentLoaded', () => {
-    const gallery = document.querySelector('.gallery');
-  
-
-
-  });
