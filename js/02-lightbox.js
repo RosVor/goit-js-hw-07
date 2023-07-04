f7d1e8a11faa86ae0ceaa587b3cba5b9d2a808fa
RosVor <ros.vorokh@gmail.com>
2023-07-04 19:13:21 +0300

    connect SLB

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,63 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+//import { galleryItems } from './gallery-items.js';
 // Change code below this line
-document.addEventListener('DOMContentLoaded', createGallery);
-function createGallery() {
-    const galleryContainer = document.querySelector('.gallery');
-  
-    const galleryMarkup = galleryItems
-      .map(item => createGalleryItem(item))
-      .join('');
-  
-    galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-  
-    const galleryLinks = document.querySelectorAll('.gallery__link');
-    galleryLinks.forEach(link => link.addEventListener('click', openLightbox));
-  }
-  
-  function openLightbox(event) {
-    event.preventDefault();
-  
-    const clickedLink = event.target.closest('.gallery__link');
-    const originalUrl = clickedLink.href;
-    const originalAlt = clickedLink.querySelector('.gallery__image').alt;
-  
-    const lightboxHtml = `
-      <div class="lightbox">
-        <div class="lightbox__overlay"></div>
-        <div class="lightbox__content">
-          <img src="${originalUrl}" alt="${originalAlt}" class="lightbox__image">
-        </div>
-      </div>
-    `;
-  
-    document.body.insertAdjacentHTML('beforeend', lightboxHtml);
-  
-    const lightbox = document.querySelector('.lightbox');
-    lightbox.addEventListener('click', closeLightbox);
-    document.addEventListener('keydown', handleKeydown);
-  }
-  
-  function closeLightbox(event) {
-    if (event.target !== event.currentTarget) return;
-  
-    const lightbox = document.querySelector('.lightbox');
-    lightbox.remove();
-  
-    document.removeEventListener('keydown', handleKeydown);
-  }
-  
-  function handleKeydown(event) {
-    if (event.key === 'Escape') {
-      closeLightbox(event);
-    }
-  }
-function createGalleryItem(item) {
-  return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${item.original}">
-        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-      </a>
-    </li>
-  `;
-}
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    captionClass: 'caption-style',
+    animationSlide: true,
+    animationSpeed: 250,
+    history: true,
+  });
+});
