b25cbf31e83f17f0d58cce0b4c3246a84a8c9b11
RosVor <ros.vorokh@gmail.com>
2023-07-04 16:00:46 +0300

    comb 01-g-js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,6 +15,29 @@ const createGalleryItem = ({ preview, original, description }) => {
     </li>
   `;
 };
+const openModal = (url) => {
+  const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+  `, {
+    onShow: (instance) => {
+      const modalImage = instance.element().querySelector('img');
+      modalImage.addEventListener('click', () => {
+        instance.close();
+      });
+    }
+  });
+
+  instance.show();
+};
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+  const target = event.target;
+
+  if (target.classList.contains('gallery__image')) {
+    const largeImageURL = target.dataset.source;
+    openModal(largeImageURL);
+  }
+});
 
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
