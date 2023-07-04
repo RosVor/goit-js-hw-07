c4789a3ac5314114a5d5b726561e489918f7dc54
RosVor <ros.vorokh@gmail.com>
2023-07-04 22:48:31 +0300

    test

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,15 +4,13 @@ const gallery = document.querySelector('.gallery');
 
 // Create gallery items
 const galleryMarkup = galleryItems
-  .map(
-    ({ original, preview, description }) => `
+  .map(({ original, preview, description }) => `
     <li class="gallery__item">
-      <a class="gallery__link" href="${original}" data-description ="${description}">
+      <a class="gallery__link" href="${original}" data-description="${description}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
       </a>
     </li>
-  `
-  )
+  `)
   .join('');
 
 // Render gallery
