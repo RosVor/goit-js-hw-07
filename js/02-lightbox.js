cc057509af0f25b1eb721b4a751d4de1329b95a2
RosVor <ros.vorokh@gmail.com>
2023-07-04 21:09:54 +0300

    exp

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,24 +3,7 @@ import { galleryItems } from './gallery-items.js';
 document.addEventListener('DOMContentLoaded', () => {
     const gallery = document.querySelector('.gallery');
   
-    // Render gallery items
-    galleryItems.forEach(item => {
-      const listItem = document.createElement('li');
-      listItem.classList.add('gallery__item');
-  
-      const link = document.createElement('a');
-      link.classList.add('gallery__link');
-      link.href = item.original;
-  
-      const image = document.createElement('img');
-      image.classList.add('gallery__image');
-      image.src = item.preview;
-      image.alt = item.description;
-  
-      link.appendChild(image);
-      listItem.appendChild(link);
-      gallery.appendChild(listItem);
-    });
+
   
     // Initialize SimpleLightbox
     new SimpleLightbox('.gallery a', {
