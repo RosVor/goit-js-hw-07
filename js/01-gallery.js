6d73d1cf3c5ea7f5e8932039d812564b63bc83f0
RosVor <ros.vorokh@gmail.com>
2023-07-04 15:41:52 +0300

    remove LB

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,9 @@
 import { galleryItems } from './gallery-items.js';
-import * as basicLightbox from 'basiclightbox';
-
+// Change code below this line
 const galleryList = document.querySelector('.gallery');
-
 const createGalleryItem = ({ preview, original, description }) => {
   return `
-  <li class="gallery__item">
+    <li class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
@@ -15,33 +13,17 @@ const createGalleryItem = ({ preview, original, description }) => {
         />
       </a>
     </li>
-     `;
+  `;
 };
-
-const openModal = (url) => {
-  const instance = basicLightbox.create(`
-    <img src="${url}" width="800" height="600">
-  `, {
-    onShow: (instance) => {
-      const modalImage = instance.element().querySelector('img');
-      modalImage.addEventListener('click', () => {
-        instance.close();
-      });
-    }
-  });
-
-  instance.show();
-};
-
-galleryList.addEventListener('click', (event) => {
-  event.preventDefault();
-  const target = event.target;
-
-  if (target.classList.contains('gallery__image')) {
-    const largeImageURL = target.dataset.source;
-    openModal(largeImageURL);
-  }
-});
 
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const galleryImages = document.querySelectorAll('.gallery__image');
+galleryImages.forEach((image) => {
+  image.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+});
+
+
